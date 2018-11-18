9598f83d08ab804b027518ce6514937b01cf2af9
DariaShantalova <darya.shantalova@phystech.edu>
2018-11-18 17:09:39 +0300

    Update 2.2 - Fibonacci.cpp

--- 2.2 - Fibonacci.cpp
+++ 2.2 - Fibonacci.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 class Fibonacci {
  public:
-  static int get(int n) {
+  static int get_last_digit(int n) {
     assert(n >= 0);
     vector<int> fib;
     if(n < 2) {
@@ -12,18 +12,56 @@ class Fibonacci {
     }else{
         fib.push_back(0);
         fib.push_back(1);
-        for(int i = 2; i < n + 1; i++){
-             fib.push_back(fib[i-1] + fib[i-2]);
+        for(int i = 2; i < n; i++){
+             fib.push_back((fib[i-1] + fib[i-2]) % 10);
         }
     }
     return fib[n];
   }
-};
+  static int get(int n) {
+      assert(n >= 0);
+      vector<int> fib(3);
+      if(n < 2) {
+          return n;
+      }else{
+          fib[0] = 0;
+          fib[1] = 1;
+          for(int i = 2; i < n + 1; i++){
+               fib.push_back(fib[i-1] + fib[i-2]);
+          }
+      }
+      return fib[n];
+   }
+   static int get_remainder(long long n, int m) {
+      assert(n >= 1);
+      assert(m >= 2);
 
-int main(void) {
-  int n;
-  std::cin >> n;
-  std::cout << Fibonacci::get(n) << std::endl;
-  return 0;
-}
+      vector<int> delimeters;
+      delimeters.push_back(0);
+      delimeters.push_back(1);
+      int period = 0;
+      for(int i = 2; i < n + 1; i++){
+    	  delimeters.push_back((delimeters[i-1] + delimeters[i-2]) % m);
+    	  period++;
+    	  if(delimeters[i] == 1 && delimeters[i-1] == 0){
+    		  return delimeters[n % period];
+    	  }
+      }
+
+      return delimeters[n];
+    }
+   static int get_remainder_no_period(long long n, int m) {
+         assert(n >= 1);
+         assert(m >= 2);
+
+         vector<int> delimeters;
+         delimeters.push_back(0);
+         delimeters.push_back(1);
+         for(int i = 2; i < n + 1; i++){
+       	  delimeters.push_back((delimeters[i-1] + delimeters[i-2]) % m);
+         }
+         return delimeters[n];
+       }
+
+};
 
